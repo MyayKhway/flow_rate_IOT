e1b0e797978b3b3d32981b94b59a94f411f56c56
KentNyiLynn <kent@pop-os.localdomain>
2024-05-23 15:39:28 +0700

    call fetch data function with parameters with Z appended for data fetch

--- flow-rate-iot/app/details/page.tsx
+++ flow-rate-iot/app/details/page.tsx
@@ -1,9 +1,10 @@
 import Navbar from '@/app/ui/navbar';
 import { LineChartComponent } from '@/app/ui/chartdetails/lineChart';
 import ControlPanel from '@/app/ui/chartdetails/ctrlPanel';
+import { fetchFlowByDateRange } from '@/app/lib/data';
 
 
-export default function Page({
+export default async function Page({
     searchParams,
 }: {
     searchParams?: {
@@ -11,11 +12,15 @@ export default function Page({
     };
 }) {
     const daterange = searchParams?.daterange || '';
+    let [from, to] = daterange.split('Z');
+    from = from || '';
+    to = to || '';
+    const chartData = await fetchFlowByDateRange(from + 'Z', to + 'Z');
     return (
         <main className="flex min-h-screen flex-col items-center justify-between">
             <Navbar />
             <ControlPanel />
-            <LineChartComponent dateRange={daterange}/>
+            <LineChartComponent chartData={chartData} />
         </main>
     );
 }
