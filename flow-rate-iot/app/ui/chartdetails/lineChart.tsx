56ebe9df9a54f7dd5fd99acc0a6038d31813759d
KentNyiLynn <kent@pop-os.localdomain>
2024-05-23 15:45:42 +0700

    a function for handling incoming data fetch result, as well as two other function for tremor LineChart component

--- flow-rate-iot/app/ui/chartdetails/lineChart.tsx
+++ flow-rate-iot/app/ui/chartdetails/lineChart.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { LineChart } from '@tremor/react';
-import { fetchLast3hrAvg } from '@/app/lib/data';
-import { useEffect, useState } from 'react';
+
 
 const chartdata = [
     {
@@ -81,19 +80,112 @@ const chartdata = [
 const dataFormatter = (number: number) =>
     `${Intl.NumberFormat('us').format(number).toString()}`;
 
+interface ChartDataType {
+    stationId: string;
+    volume: number,
+    time: Date;
+    stationFrom: {
+        name: string;
+    }
+}
 
-export function LineChartComponent({ dateRange }: { dateRange: string }) {
-    // TODO respond to dateRange data
+interface ResultDataType {
+    [key: string]: string;
+}
+
+
+const findInObjArr = (results: ResultDataType[], val: string) => {
+    for (const row of results) {
+        if (val == row.time) {
+            // there is already one object with time value
+            return row;
+        }
+    } return false;
+}
+
+export function LineChartComponent({ chartData }: { chartData: ChartDataType[] }) {
+    // chartData will look like this
+    // {
+    // stationId: '0000',
+    // volume: 748.5275216399856,
+    // time: 2024-04-30T17:00:00.000Z,
+    //stationFrom: { name: 'Station0' }
+    //}
+
+
+    const parseChartData = (data: ChartDataType[]) => {
+        // ASSUMPTION!!!! is that there is no two volume data for same station at the same time
+        // result will save data in the from LineChart component will accept which is 
+        // { 
+        // xAxisname: xAxisval,
+        // Series0: data0 
+        // Series1: data1 
+        // ...    : ...
+        // Seriesn: datan 
+        // }
+        let results: ResultDataType[] = [];
+        for (const row of data) {
+            let found = findInObjArr(results, row.time.toISOString());
+            if (found) {
+                // there is already an entry with that time xAxis value
+                found[row.stationFrom.name] = Math.round(row.volume).toString();
+            } else {
+                // construct a new object and append to results
+                const stationName = row.stationFrom.name;
+                const new_obj: ResultDataType = {
+                    'time': row.time.toISOString(),
+                }
+                new_obj[stationName] = Math.round(row.volume).toString();
+                results.push(new_obj);
+            }
+        }
+        for (let i = 0; i < 10; i++) {
+            console.log(results[i]);
+        }
+        return results;
+    }
+
+
+    const getRandomSubArray = (arr: string[], size: number) => {
+        let shuffled = arr.slice(0), i = arr.length, temp, index;
+        while (i--) {
+            index = Math.floor((i + 1) * Math.random());
+            temp = shuffled[index];
+            shuffled[index] = shuffled[i];
+            shuffled[i] = temp;
+        }
+        return shuffled.slice(0, size);
+    }
+
+    const tailWindColors = [
+        'slate', 'gray',
+        'zinc', 'neutral',
+        'stone', 'red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 'teal', 'cyan', 'sky', 'blue',
+        'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose',
+    ]
+
+
+    const chartDataParsed = parseChartData(chartData);
+    let categories: string[] = [];
+    // ugly, TODO fix this
+    if (chartDataParsed != undefined && chartDataParsed != null && chartDataParsed[0] != undefined && chartDataParsed[0] != null) {
+        let keys = Object.keys(chartDataParsed[0]);
+        const index = keys.indexOf("time");
+        keys.splice(index, 1);
+        categories = keys;
+    }
+
+    const randomColors = getRandomSubArray(tailWindColors, 7);
 
     return (
         <LineChart
             className="h-80"
-            data={chartdata}
-            index="date"
-            categories={['Station0', 'Station1', 'Station3']}
-            colors={['indigo', 'rose', 'blue']}
+            data={chartDataParsed}
+            index="time"
+            categories={categories}
+            colors={randomColors}
             valueFormatter={dataFormatter}
-            yAxisWidth={60}
+            yAxisWidth={100}
             onValueChange={(v) => console.log(v)}
         />
     );
