e4ca696cafd93e5f8d33ad5cc599aded7f8754d8
KentNyiLynn <kent@pop-os.localdomain>
2024-05-23 15:41:14 +0700

    implement data fetch given a timerange

--- flow-rate-iot/app/lib/data.ts
+++ flow-rate-iot/app/lib/data.ts
@@ -18,7 +18,7 @@ export const fetchStations = async () => {
 }
 
 export async function fetchLast3hrAvg() {
-    //TODO remove this hardcoded value and replace with Date.now()-3hours after database peridic inputs
+    //TODO remove this hardcoded value and replace with Date.now()-3hours after database periodic inputs
     const last3Hr = new Date("2024-05-13 21:15:00:00.00").toISOString();
     const flows = await prisma.flow.findMany({
         where: {
@@ -45,7 +45,41 @@ export async function fetchLast3hrAvg() {
             results.push(new_obj);
         }
     }
-    console.log(flows.length);
-    console.log(results);
+    return results;
+}
+
+
+export async function fetchFlowByDateRange(from: string, to: string) {
+    // if from or to are empty strings, display last day data
+    if (from == 'Z' || to == 'Z') {
+        from = new Date(new Date().setHours(0)).toISOString();
+        to = new Date().toISOString();
+    }
+    const results = await prisma.flow.findMany({
+            include : {
+                stationFrom: {
+                    select: {
+                        name: true,
+                    }
+                }
+            },
+            where : {
+                time: {
+                    gte: from,
+                    lte: to,
+                }
+            },
+            orderBy : {
+                time: 'asc' 
+            },
+        });
+    console.log(`returned results have length of: ${results.length}`);
+    // returned results will look like this
+      // {
+      // stationId: '0000',
+      // volume: 748.5275216399856,
+      // time: 2024-04-30T17:00:00.000Z,
+      //stationFrom: { name: 'Station0' }
+        // }
     return results;
 }
