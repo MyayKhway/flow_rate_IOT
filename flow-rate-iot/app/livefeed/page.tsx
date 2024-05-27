880aff387de62797ae6c624fa121b67f8f7d6251
KentNyiLynn <kent@pop-os.localdomain>
2024-05-27 13:49:24 +0700

    make a wrapper for video player

--- flow-rate-iot/app/livefeed/page.tsx
+++ flow-rate-iot/app/livefeed/page.tsx
@@ -1,18 +1,13 @@
 import Navbar from '@/app/ui/navbar';
 import { fetchStations } from '@/app/lib/data';
-import VideoPlayer from '@/app/ui/livevideo/videoPlayer';
+import VideoWrapper from '@/app/ui/livevideo/videoWrapper';
 
 export default async function Home() {
     const stations = await fetchStations();
     return (
         <main className="flex min-h-screen flex-col items-center justify-between">
             <Navbar />
-            <div className="grid grid-cols-3 gap-0 min-h-screen">
-                <VideoPlayer stations={stations} />
-            </div>
-            <svg className="size-20 absolute right-10 bottom-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-            </svg>
+            <VideoWrapper stations={stations} />
         </main>
     );
 }
